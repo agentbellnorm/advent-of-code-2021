e35d476686a90c552cc3698adabaf078a67abc61
Morgan Bentell <morgan.bentell@gmail.com>
2021-12-05 16:04:22 +0100

    include dist folder

--- craco.config.js
+++ craco.config.js
@@ -1,30 +1,36 @@
-const path = require('path');
-const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+const path = require("path");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
-
   webpack: {
     plugins: {
       add: [
         new WasmPackPlugin({
-          crateDirectory: path.resolve(__dirname, './src/rust'),
-          outDir: path.resolve(__dirname, "./src/rust/pkg")
-
-        })
-      ]
+          crateDirectory: path.resolve(__dirname, "./src/rust"),
+          outDir: path.resolve(__dirname, "./src/rust/pkg"),
+        }),
+      ],
     },
-    configure: (webpackPlugin) => {
-      webpackPlugin.resolve.extensions.push('.wasm');
+    configure: (webpackConfig, { env, paths }) => {
+      webpackConfig.resolve.extensions.push(".wasm");
 
-      webpackPlugin.module.rules.forEach(rule => {
-        (rule.oneOf || []).forEach(oneOf => {
+      webpackConfig.module.rules.forEach((rule) => {
+        (rule.oneOf || []).forEach((oneOf) => {
           if (oneOf.loader && oneOf.loader.indexOf("file-loader") >= 0) {
             // Make file-loader ignore WASM files
             oneOf.exclude.push(/\.wasm$/);
           }
         });
       });
-      return webpackPlugin;
-    }
-  }
+
+      console.log(webpackConfig);
+
+      paths.appBuild = webpackConfig.output.path = path.resolve(
+        __dirname,
+        "dist"
+      );
+
+      return webpackConfig;
+    },
+  },
 };
